a58df481141fbc46e6625709109a9ebd4b350db2
Paul Yabsley <yabsleyp@gmail.com>
2016-12-16 10:56:20 +0000

    Add array method functionality

--- array-cardio-day-2/index.php
+++ array-cardio-day-2/index.php
@@ -27,17 +27,46 @@
 
 	// Some and Every Checks
 	// Array.prototype.some() // is at least one person 19?
+	// const isAdult = people.some(function(person) {
+	// 	const currentYear = (new Date()).getFullYear();
+	// 	if (currentYear - person.year >= 19) {
+	// 		return true;
+	// 	}
+	// });
+	// Or do it hot shot style
+	const isAdult = people.some(person => ((new Date()).getFullYear()) - person.year >= 19);
+	console.log({isAdult});
 
 	// Array.prototype.every() // is everyone 19?
+	const allAdults = people.every(person => ((new Date()).getFullYear()) - person.year >= 19);
+	console.log({allAdults});
 
 	// Array.prototype.find()
-
 	// Find is like filter, but instead returns just the one you are looking for
 	// find the comment with the ID of 823423
 	// Array.prototype.findIndex()
+	// const comment = comments.find(function(comment) {
+	// 	if (comment.id === 823423) {
+	// 		return true;
+	// 	}
+	// });
+	// Or the same thing but hot shot
+	const comment = comments.find(comment => comment.id === 823423);
+	console.log(comment);
 
 	// Find the comment with this ID
 	// delete the comment with the ID of 823423
+	const index = comments.findIndex(comment => comment.id === 823423);
+	// console.log(index);
+	// comments.splice(index, 1);
+	console.table(comments);
+
+	const newComments = [
+		...comments.slice(0, index),
+		...comments.slice(index + 1)
+	];
+	console.table(newComments);
+
 </script>
 </body>
 </html>
